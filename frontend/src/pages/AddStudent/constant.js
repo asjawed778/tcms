83948bd1c57ccf72d9964c986fba8065596c2c9d
Amit Kumar Ranjan <122173165+iamakr97@users.noreply.github.com>
2025-04-15 22:08:25 +0530

    Delete frontend directory

--- frontend/src/pages/AddStudent/constant.js
+++
@@ -1,1 +0,0 @@
-export const GENDER_OPTIONS = ["Male", "Female", "Other"];
--- frontend/src/pages/dashboard/index.jsx
+++
@@ -1,8 +0,0 @@
-const DashboardPage = () => {
-    return(
-        <>
-            <h1 className="text-primary text-center m-25">Welcome to dashboard.....</h1>
-        </>
-    )
-}
-export default DashboardPage;
